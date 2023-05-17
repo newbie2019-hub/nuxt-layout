00042837677848764c4b80cc1c422f94672c2b5e
Yvan Sabay <sabayyvan2018@gmail.com>
2023-05-18 01:26:27 +0800

    added components - FormInputContainer and ProfileCardContainer

---
+++ components/FormInputContainer.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="border pt-4 pb-3 px-4 rounded position-relative">
+		<div class="text">
+			<p class="fs-6 fw-semibold">{{ text }}</p>
+		</div>
+		<div class="switch">
+			<div class="form-check form-switch">
+				<input
+					class="form-check-input bg-warning"
+					type="checkbox"
+					role="switch"
+					id="flexSwitchCheckDefault"
+				/>
+			</div>
+		</div>
+		<slot />
+	</div>
+</template>
+<script setup>
+defineProps({
+	text: {
+		type: String,
+    required: true
+	},
+});
+</script>
+<style scoped>
+.text {
+	position: absolute;
+	top: -13px;
+	left: 15px;
+	background-color: white;
+	padding: 0 6px;
+}
+
+.switch {
+	position: absolute;
+	top: -13px;
+	right: 15px;
+	background-color: white;
+	padding: 0 0px 0 8px;
+}
+</style>
---
+++ components/ProfileCardContainer.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="card">
+		<div class="card-body">
+			<div class="d-flex justify-content-center">
+				<img
+					src="https://robohash.org/frontend-casting"
+					alt=""
+					class="rounded-circle"
+					height="140"
+				/>
+			</div>
+			<h4 class="text-center mt-4">
+				Johny_Official
+				<a
+					href=""
+					class="text-black"
+					><i class="fa fa-solid fa-pencil fa-xs"></i
+				></a>
+			</h4>
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item">
+					<div class="d-flex justify-content-between">
+						<p>Sponsor Level</p>
+						<span class="badge bg-info rounded-pill">57</span>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="d-flex justify-content-between">
+						<p>Sponsor Plan</p>
+						<div>
+							Platinum
+							<span class="badge bg-info rounded-pill">
+								<i class="fa-solid fa-trophy fa-sm"></i>
+							</span>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="d-flex justify-content-between">
+						<p>Verified</p>
+						<span class="badge bg-danger rounded-pill">x</span>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="d-flex justify-content-between">
+						<p>Ratings</p>
+						<div>
+							<span class="badge bg-info rounded-pill"></span>
+							5.0
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="d-flex justify-content-between">
+						<p>Completed Contracts</p>
+						<p>14</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+<script setup></script>
+<style scoped>
+p {
+	margin-bottom: 0;
+}
+</style>
